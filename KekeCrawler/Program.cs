56655097e5b559ca020340fc7f65f370df979196
Cao <Lu_Cao@APAC.DELL.COM>
2018-01-31 07:42:01 +0800

    add initial files

--- KekeCrawler/Program.cs
+++ KekeCrawler/Program.cs
@@ -1,14 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Abot.Core;
+using Abot.Crawler;
+using Abot.Poco;
+using System;
+using System.Net;
 
 namespace KekeCrawler
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            CrawlConfiguration crawlConfig = AbotConfigurationSectionHandler.LoadFromXml().Convert();
+            crawlConfig.MaxConcurrentThreads = 5;//this overrides the config value
+            crawlConfig.MaxCrawlDepth = 0;
+            PoliteWebCrawler crawler = new PoliteWebCrawler();
+            crawler.PageCrawlStartingAsync += crawler_ProcessPageCrawlStarting;
+            crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
+            crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowed;
+            crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed;
+
+            CrawlResult result = crawler.Crawl(new Uri("http://www.kekenet.com/Article/15410/List_58.shtml"));
+            if (result.ErrorOccurred)
+                Console.WriteLine("Crawl of {0} completed with error: {1}", result.RootUri.AbsoluteUri, result.ErrorException.Message);
+            else
+                Console.WriteLine("Crawl of {0} completed without error.", result.RootUri.AbsoluteUri);
+        }
+
+        private static void crawler_PageLinksCrawlDisallowed(object sender, PageLinksCrawlDisallowedArgs e)
+        {
+        }
+
+        private static void crawler_PageCrawlDisallowed(object sender, PageCrawlDisallowedArgs e)
+        {
+        }
+
+        private static void crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+
+            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+                Console.WriteLine("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
+            else
+                Console.WriteLine("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
+
+            if (string.IsNullOrEmpty(crawledPage.Content.Text))
+                Console.WriteLine("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
+
+            var htmlAgilityPackDocument = crawledPage.HtmlDocument; //Html Agility Pack parser
+            var angleSharpHtmlDocument = crawledPage.AngleSharpHtmlDocument; //AngleSharp parser
+        }
+
+        private static void crawler_ProcessPageCrawlStarting(object sender, PageCrawlStartingArgs e)
         {
         }
     }
